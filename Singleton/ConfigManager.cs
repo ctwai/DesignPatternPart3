c452e913dda25324886a80a93647485631194c6a
Dave Wai <dave.wai@outlook.com>
2021-08-26 23:13:57 +1000

    Create the program without singleton
    - Create the problem here to show why we need a class with a single instance only

---
+++ Singleton/ConfigManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton
+{
+    public class ConfigManager
+    {
+        private Dictionary<string, object> settings = new Dictionary<string, object>();
+
+        public void Set(string key, object value)
+        {
+            settings.Add(key, value);
+        }
+
+        public object Get(string key)
+        {
+            return settings[key];
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -6,7 +6,26 @@ namespace Singleton
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a class with a single instance - like configManager/Log class
+            // Issue - we are creating two instance of ConfigManager here
+            ConfigManager manager = new ConfigManager();
+            manager.Set("name", "Dave");
+
+            ConfigManager other = new ConfigManager();
+            Console.WriteLine(other.Get("name"));
+
+            Console.ReadLine();
+
+            // Todo
+            // We need to
+            // 1. set the constructor to private so outside class cannot instantiate
+            // the configmanager object
+            // 2. Create a private static object inside the config manager so it can craete the
+            // object inside the ConfigManager Class (Not from the outside class)
+
+
+
+
         }
     }
 }
