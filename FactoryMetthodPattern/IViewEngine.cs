8b9df2990e70679d2c88ed72771c13d78f141865
Dave Wai <dave.wai@outlook.com>
2021-08-30 23:24:10 +1000

    Factor Method
    Defer the object creation to the sub class
    - in this case, we provide the implementation in the controller base class - CreateViewEngine method and use the subclass SharpController.cs to override the implementation of the base class (Controller.cs).  
    
    When we create a new controller, we can choose to inherit from the Controller base class or SharpController concrete class which also implement the Controller as a base class but overwrite it CreateViewEngine() to return a new SharpViewEngine() object.

---
+++ FactoryMetthodPattern/IViewEngine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryMetthodPattern
+{
+    public interface IViewEngine
+    {
+        string Render(string viewName, Dictionary<string, object> context);
+    }
+}
---
+++ FactoryMetthodPattern/Matcha/MatchaViewEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryMetthodPattern
+{
+    public class MatchaViewEngine : IViewEngine
+    {
+        // Extract the method here to interface so we can pass it as 
+        // a parameter to the Controller class
+        public string Render(String viewName, Dictionary<string, object> context)
+        {
+            return "View Return by Matcha";
+        }
+    }
+}
