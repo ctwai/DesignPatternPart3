532d01cbb2b2e63d7e259b109c71a23bae39323a
Dave Wai <dave.wai@outlook.com>
2021-09-10 07:53:38 +1000

    BuilderPattern
    Initial Commit

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BuilderPattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // BuildPattern
+            // To separate the construction of an object from its representation
+
+            // in this example we separate the construction (export method) from the
+            // type we want to export (concrete implementation) - like PDF/Excel/Word/Movie etc
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
